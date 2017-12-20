3b54f11c28b7d79d25f79001357913ac1965eec8
nouffriz <34641721+nouffriz@users.noreply.github.com>
2017-12-20 10:23:09 +0000

    Create 1.c

---
+++ codeketa/set1/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+					int a;
+					scanf("%d",&a);
+					if(a==0)
+					{
+						printf("zero \n",a);
+					}
+					else if(a>0)
+					{
+						printf("positive \n",a);
+					}
+					else
+					printf("negative",a);
+					
+	return 0;
+}
