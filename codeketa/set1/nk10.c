d9cb32a0a9ea0fd212d80e1f6c0a8380c23c971b
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-01-09 14:35:41 +0530

    Create nk10.c

---
+++ codeketa/set1/nk10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void) {
+					int n[10];
+					int i,a;
+					int sum;
+					scanf("%d",&a);
+					for( i=1;i<=a;i++)
+					{
+						scanf("%d",&n[i]);
+						sum=n[1]+n[2];
+					}
+					printf("%d",sum);
+					
+				
+	return 0;
+}
