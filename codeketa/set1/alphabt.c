0d4eff25c12583fd2c3484ad2011c171561cdf93
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-01-09 12:45:34 +0530

    Create alphabt.c

---
+++ codeketa/set1/alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+					char ch;
+					scanf("%s",&ch);
+						if((ch >=97 && ch <=122)||(ch >=65 && ch <=90))
+						{
+							printf("alphabet",ch);
+						}
+						else
+						printf("no",ch);
+	return 0;
+}
