aa2e56a6d574709486cfa546c49fa43e7d635028
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-01-11 10:28:20 +0530

    Create oddin.c

---
+++ codeketa/set2/oddin.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+					int s;
+					int e;
+					scanf("%d",&s);
+					scanf("%d",&e);
+					for(int i=2;i<e;i++)
+					
+					{
+						if(i%2!=0)
+						printf(" %d",i);
+					}
+	return 0;
+}
