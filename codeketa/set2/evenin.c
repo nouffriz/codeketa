a8a12ae377027b1daf9f4bee499154558e1aa232
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-01-11 10:34:29 +0530

    Create evenin.c

---
+++ codeketa/set2/evenin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+					int s;
+					int e;
+					scanf("%d",&s);
+					scanf("%d",&e);
+					for(int i=7;i<e;i++)
+					
+					{
+						if(i%2==0)
+						printf(" %d",i);
+					}
+	return 0;
+}
