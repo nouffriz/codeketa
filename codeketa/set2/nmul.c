aa09c4ecd779df2e38ac9e719597bcedcb062d89
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-01-18 20:10:10 +0530

    Create nmul.c

---
+++ codeketa/set2/nmul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+						int n;
+						int sum;
+						scanf("%d",&n);
+						for(int i=1;i<=n;i++)
+						{
+							sum=5*i;
+							printf("%d\n",sum);
+						}
+	return 0;
+}
