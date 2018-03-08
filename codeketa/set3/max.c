1e11f368552c9ec1ab57c1b4204d1161314b9ca5
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-03-08 09:32:46 +0530

    Create max.c

---
+++ codeketa/set3/max.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+					int size;
+					int a[10];
+					int i;
+					int temp;
+					scanf("%d",&size);
+					for(i = 0;i < size;i++){
+						scanf("%d",&a[i]);
+					}
+					temp=a[0];
+					for(i = 1;i < size; i++)
+					if(temp<a[i])
+					{
+						temp=a[i];
+					}
+					printf("%d",temp);
+					
+					
+	return 0;
+}
