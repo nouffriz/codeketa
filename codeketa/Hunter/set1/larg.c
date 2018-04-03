15d41233fd1c4f9d5521f172a28fdb09c1f7937f
nouffriz <34641721+nouffriz@users.noreply.github.com>
2018-04-03 16:00:43 +0530

    Create larg.c

---
+++ codeketa/Hunter/set1/larg.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+	                    int i;
+	                    int n;
+	                    int j;
+	                    int a[n];
+	                    int temp;
+	                    scanf("%d",&n);
+	                    for(i=0;i<n;i++)
+	                    {
+	                    	scanf("%d",&a[i]);
+	                    }
+	                    for(i=0;i<n;i++)
+	                    {
+	                    	for( j=i+1;j<n;j++)
+	                    	{
+	            		 if(a[i]<a[j])
+	                        {
+	                    		
+	                    		  temp=a[i];
+	                    		  a[i]=a[j];
+	                    		  a[j]=temp;
+	                    		}
+	                    	}
+	                    }
+	                    for(i=0;i<n;i++)
+	                    {
+	                    	printf("%d",a[i]);	
+	                    }
+	                    
+	return 0;
+}
